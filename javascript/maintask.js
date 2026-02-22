5ea4d34baa473027effd14fb47ac826771e0f3ea
Md Ibrahim Khalil <developeribrahim13@gmail.com>
2026-02-22 18:12:16 +0600

    achieve 100% functionality for interview and rejected toggles with dynamic renderin

--- javascript/maintask.js
+++ javascript/maintask.js
@@ -16,7 +16,7 @@ document.getElementById('all-cards').addEventListener('click',function(event){
     card.querySelector('.bdg').innerText = 'NOT APPLIED'; 
     interviewPart = interviewPart.filter(elmnt => elmnt.company !== company);
     count();
-    rendering();
+    rendering('interview-section',interviewPart);
     return;
     }
     card.querySelector('.bdg').innerText = 'INTERVIEW';
@@ -37,39 +37,80 @@ document.getElementById('all-cards').addEventListener('click',function(event){
     {
         interviewPart.push(cardElement);
         count();
-        rendering();
+        rendering('interview-section',interviewPart);
+    }
+}
+
+   else if(event.target.classList.contains('rjctd'))
+    {
+     const card = event.target.closest('.crd')
+    
+    const company = card.querySelector('.company-name').innerText;
+    const position =card.querySelector('.position').innerText;
+    const lts = card.querySelector('.lts').innerText;
+    const description = card.querySelector('.description').innerText;
+    let badge = card.querySelector('.bdg').innerText;
+    if(badge=='REJECTED')
+    {
+    card.querySelector('.bdg').classList.remove('badge-outline', 'badge-success');        
+    card.querySelector('.bdg').classList.add('badge-primary', 'text-[#002c5c]');
+    card.querySelector('.bdg').innerText = 'NOT APPLIED'; 
+    rejectedPart = rejectedPart.filter(elmnt => elmnt.company !== company);
+    count();
+    rendering('rejected-section', rejectedPart);
+    return;
+    }
+    card.querySelector('.bdg').innerText = 'REJECTED';
+    card.querySelector('.bdg').classList.remove('badge-primary', 'text-[#002c5c]');
+    card.querySelector('.bdg').classList.add('badge-outline', 'badge-error');
+
+    const cardElement = {
+        company,
+        position,
+        lts,
+        badge:'REJECTED',
+        description
+    };
+
+    const exist = rejectedPart.find(item => item.company == cardElement.company);
+
+    if(!exist)
+    {
+        rejectedPart.push(cardElement);
+        count();
+        rendering('rejected-section', rejectedPart);
     }
 }
 
 
 });
 
-function rendering(){
-    document.getElementById('interview-section').innerHTML = '';
-    for(let intr of interviewPart){
+function rendering(id, arry){
+    document.getElementById(id).innerHTML = '';
+    for(let el of arry){
         const div = document.createElement('div');
         div.className = 'bg-white p-4 rounded-md space-y-3 crd';
         div.innerHTML = `
             <div class="flex justify-between">
 
                     <div>
-                        <h3 class="company-name font-medium text-lg leading-9 text-[#002c5c]">${intr.company}</h3>
-                        <p class="position text-gray-400">${intr.position}</p>
+                        <h3 class="company-name font-medium text-lg leading-9 text-[#002c5c]">${el.company}</h3>
+                        <p class="position text-gray-400">${el.position}</p>
                     </div>
 
                     <button class="btn bg-white rounded-full p-3"><i class="fa-solid fa-trash"></i></button>
                 </div>
 
-                <p class="lts text-gray-400">${intr.lts}</p>
-                <div class="badge badge-soft badge-primary text-[#002c5c] pt-5 pb-4 font-medium bdg">${intr.badge}</div>
+                <p class="lts text-gray-400">${el.lts}</p>
+                <div class="badge badge-soft badge-primary text-[#002c5c] pt-5 pb-4 font-medium bdg">${el.badge}</div>
 
-                <p class="description text-gray-600">${intr.description}</p>
+                <p class="description text-gray-600">${el.description}</p>
                 <div class="space-x-1">
                     <button class="btn btn-outline btn-success intrvw">INTERVIEW</button>
                     <button class="btn btn-outline btn-error rjctd">REJECTED</button>
                 </div>
         `;
-        document.getElementById('interview-section').appendChild(div);
+        document.getElementById(id).appendChild(div);
     }
 };
 
